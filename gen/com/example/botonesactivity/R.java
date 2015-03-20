7581c71a5195eaa0e4e22747f56fdf2664d71776
Guilleves <guilleves@gmail.com>
2015-03-19 21:25:05 -0300

    added all files from the working project

--- gen/com/example/botonesactivity/R.java
+++ gen/com/example/botonesactivity/R.java
@@ -13,12 +13,40 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int EditText01=0x7f060010;
+        public static final int EditText02=0x7f06000d;
+        public static final int EditText03=0x7f06000a;
+        public static final int EditText04=0x7f060007;
+        public static final int EditText05=0x7f060004;
+        public static final int TableRow01=0x7f06000e;
+        public static final int TableRow02=0x7f06000b;
+        public static final int TableRow03=0x7f060008;
+        public static final int TableRow04=0x7f060005;
+        public static final int TableRow05=0x7f060002;
+        public static final int TextView01=0x7f06000f;
+        public static final int TextView02=0x7f06000c;
+        public static final int TextView03=0x7f060009;
+        public static final int TextView04=0x7f060006;
+        public static final int TextView05=0x7f060003;
+        public static final int editText1=0x7f060013;
+        public static final int scrollView1=0x7f060000;
+        public static final int tableLayout1=0x7f060001;
+        public static final int tableRow1=0x7f060011;
+        public static final int textView1=0x7f060012;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int det=0x7f040007;
         public static final int hello_world=0x7f040001;
+        public static final int mail=0x7f040004;
+        public static final int name=0x7f040002;
+        public static final int pw=0x7f040005;
+        public static final int rpw=0x7f040006;
+        public static final int surname=0x7f040003;
     }
     public static final class style {
         /** 
